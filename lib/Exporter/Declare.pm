af12dfd98951b0e191fe2ba0636a64a6969335e8
Chad Granum <exodist7@gmail.com>
2010-04-05 18:53:31 -0700

    Initial Commit

---
+++ lib/Exporter/Declare.pm
@@ -0,0 +1,167 @@
+package Exporter::Declare;
+use strict;
+use warnings;
+
+use Carp;
+use Scalar::Util qw/blessed/;
+
+our @EXPORT = qw/export/;
+our $VERSION = 0.001;
+
+sub import {
+    my $class = shift;
+    my $caller = caller;
+    my ( $imports, $specs ) = $class->_import_args( @_ );
+    $class->export_to( $caller, $specs->{prefix} || undef, @$imports );
+
+    my $base = $specs->{extend} ? $class : 'Exporter::Declare::Base';
+
+    no strict 'refs';
+    push @{ $caller . '::ISA' } => $base
+        unless grep { $_ eq $base } @{ $caller . '::ISA' };
+}
+
+sub _import_args {
+    my $class = shift;
+    my ( @imports, %specs );
+    for my $item ( @_ ) {
+        if ( $item =~ m/^:([^:]*)(?::(.*))?$/ ) {
+            $specs{ $1 } = $2 || 1;
+        }
+        else {
+            push @imports => $item;
+        }
+    }
+    return( \@imports, \%specs );
+}
+
+sub exports {
+    my $class = shift;
+    no strict 'refs';
+    return {
+        ( map { $_ => $_ } @{ $class . '::EXPORT' }),
+        %{ $class . '::EXPORT' },
+    };
+}
+
+sub export_to {
+    my $class = shift;
+    my ( $dest, $prefix, @list ) = @_;
+    my $exports = $class->exports;
+    for my $name ( @list || keys %$exports ) {
+        my $sub = $exports->{ $name };
+        $sub = $class->can( $sub ) unless ref $sub eq 'CODE';
+
+        croak( "Could not find sub '$name' in $class for export" )
+            unless ref($sub) eq 'CODE';
+
+        $name = $prefix . $name if $prefix;
+        no strict 'refs';
+        *{ $dest . '::' . $name } = $sub;
+    }
+}
+
+sub export {
+    my $exporter;
+    $exporter = shift( @_ ) if @_ > 2;
+    $exporter = blessed( $exporter ) || $exporter || caller;
+
+    my ( $name, $sub ) = @_;
+    croak( "You must provide a name to export()" )
+        unless $name;
+    $sub ||= $exporter->can( $name );
+    croak( "No code found in '$exporter' for exported sub '$name'" )
+        unless $sub;
+
+    no strict 'refs';
+    my $export = \%{ $exporter . '::EXPORT' };
+    $export->{ $name } = $sub;
+}
+
+package Exporter::Declare::Base;
+use strict;
+use warnings;
+use base 'Exporter::Declare';
+
+sub import {
+    my $class = shift;
+    my $caller = caller;
+    my ( $imports, $specs ) = $class->_import_args( @_ );
+    $class->export_to( $caller, $specs->{prefix} || undef, @$imports );
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Exporter::Declare - Declarative function exporting
+
+=head1 DESCRIPTION
+
+Declarative function exporting. You can export subs as usualy with @EXPORT, or
+export anonymous subs under whatever name you want. You can also extend
+Exporter::Declare very easily. You can also add an export from outside the
+package using the export() class method on it.
+
+=head1 SYNOPSYS
+
+Basic usage:
+
+    package MyPackage;
+    use strict;
+    use warnings;
+    use Exporter::Declare;
+
+    # works as expected
+    our @EXPORT = qw/a/;
+
+    # Declare an anonymous export
+    export b => sub { 'b' };
+
+    export 'c';
+    sub c { 'c' }
+    sub a { 'a' }
+
+    1;
+
+Extending:
+
+    package MyExporterDeclare;
+    use strict;
+    use warnings;
+    use Exporter::Declare ':extend';
+
+    export my_export => sub {
+        my ( $name, $sub ) = @_;
+        export( $name, $sub );
+    };
+
+Import with a prefix:
+
+    package MyThing;
+    use strict;
+    use warnings;
+    use MyThingThatExports ':prefix:myprefix';
+
+Import a list of subs only:
+
+    package MyThing;
+    use strict;
+    use warnings;
+    use MyThingThatExports qw/ sub_a sub_b /;
+
+=head1 AUTHORS
+
+Chad Granum L<exodist7@gmail.com>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2010 Chad Granum
+
+Exporter-Declare is free software; Standard perl licence.
+
+Exporter-Declare is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the license for more details.
---
+++ t/Exporter.t
@@ -0,0 +1,109 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Test::More;
+use Test::Exception;
+
+use_ok( "Exporter::Declare" );
+
+{
+    package Extended;
+    use strict;
+    use warnings;
+    use Exporter::Declare ':extend';
+
+    our @EXPORT = qw/a export/;
+
+    export b => sub { 'b' };
+    export 'c';
+
+    sub a { 'a' }
+    sub c { 'c' }
+
+    package UseExtended;
+    use strict;
+    use warnings;
+    Extended->import;
+
+    export( 'c' => sub { 'c' });
+
+    package UseExtendedExtended;
+    use strict;
+    use warnings;
+    UseExtended->import();
+
+    package UsePrefix;
+    use strict;
+    use warnings;
+    UseExtended->import( ':prefix:blah_' );
+
+    package NormalUse;
+    use strict;
+    use warnings;
+    use Exporter::Declare;
+
+    our @EXPORT = qw/f/;
+
+    export e => sub { 'e' };
+
+    sub f { 'f' }
+}
+
+can_ok( 'Extended', 'export' );
+isa_ok( 'Extended', 'Exporter::Declare' );
+is_deeply(
+    [ sort keys %{ Extended->exports }],
+    [qw/ a b c export /],
+    "exports"
+);
+
+can_ok( 'UseExtended', 'export', 'a', 'b', 'c' );
+ok( !UseExtended->isa( 'Extended' ), "Not an extended" );
+isa_ok( 'UseExtended', 'Exporter::Declare' );
+isa_ok( 'UseExtended', 'Exporter::Declare::Base' );
+is_deeply(
+    [ keys %{ UseExtended->exports }],
+    [ 'c' ],
+    "export",
+);
+UseExtended->export( 'd' => sub { 'd' });
+is_deeply(
+    [ keys %{ UseExtended->exports }],
+    [ 'c', 'd' ],
+    "export as class method",
+);
+
+can_ok( 'NormalUse', 'export' );
+ok( !NormalUse->isa( 'Extended' ), "Not an extended" );
+isa_ok( 'UseExtended', 'Exporter::Declare' );
+isa_ok( 'UseExtended', 'Exporter::Declare::Base' );
+is_deeply(
+    [ keys %{ NormalUse->exports }],
+    [ 'e', 'f' ],
+    "Exports in normal use",
+);
+
+throws_ok { NormalUse::export() }
+    qr/You must provide a name to export\(\)/,
+    "Must provide a name";
+
+throws_ok { NormalUse::export( 'bubba' )}
+    qr/No code found in 'main' for exported sub 'bubba'/,
+    "Must have sub when adding export";
+
+push @NormalUse::EXPORT => 'apple';
+throws_ok { NormalUse->export_to( 'xxx' )}
+    qr/Could not find sub 'apple' in NormalUse for export/,
+    "Must have sub to export";
+
+can_ok( 'UseExtendedExtended', 'c' );
+ok( !UseExtendedExtended->isa( 'Extended' ), "Not an extended" );
+ok( !UseExtendedExtended->isa( 'Extended::Declare' ), "Not a declare" );
+ok( !UseExtendedExtended->isa( 'Extended::DeclareBase' ), "Not a declarebase" );
+ok( !UseExtendedExtended->can( 'export' ), "Can't export" );
+
+ok( !UsePrefix->can( 'c' ), "No c" );
+can_ok( 'UsePrefix', 'blah_c' );
+
+done_testing;
